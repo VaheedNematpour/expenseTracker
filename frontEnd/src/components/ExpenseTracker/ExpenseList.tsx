a1430c7395f0e78dd8048187bee2ee32855e549f
Vaheed Nematpour <vaheed.nematpour@gmail.com>
2024-10-25 14:47:54 +0330

    Adding Style to our ExpenseList component

--- frontEnd/src/components/ExpenseTracker/ExpenseList.tsx
+++ frontEnd/src/components/ExpenseTracker/ExpenseList.tsx
@@ -13,25 +13,55 @@ interface Props {
 const ExpenseList = ({ expenses, onDleteExpense }: Props) => {
   return (
     <>
-      <h2>Expense List</h2>
+      <h2 className="text-3xl text-gray-800 font-bold">Expense List</h2>
 
-      <table>
+      <table className="w-full text-left">
         <thead>
           <tr>
-            <th>Title</th>
-            <th>Category</th>
-            <th>Amount</th>
-            <th></th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-xl text-gray-600 font-bold"
+            >
+              Title
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-xl text-gray-600 font-bold"
+            >
+              Category
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-xl text-gray-600 font-bold"
+            >
+              Amount
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-xl text-gray-600 font-bold"
+            ></th>
           </tr>
         </thead>
         <tbody>
           {expenses.map((expense) => (
             <tr key={expense.id}>
-              <td>{expense.title}</td>
-              <td>{expense.category}</td>
-              <td>{expense.amount}</td>
-              <td>
-                <button onClick={() => onDleteExpense(expense.id)}>
+              <td
+                scope="row"
+                className="px-6 py-4 text-xl text-gray-600 font-medium"
+              >
+                {expense.title}
+              </td>
+              <td scope="row" className="px-6 py-4 text-xl text-gray-600">
+                {expense.category}
+              </td>
+              <td scope="row" className="px-6 py-4 text-xl text-gray-600">
+                {expense.amount}
+              </td>
+              <td scope="row" className="px-6 py-4">
+                <button
+                  onClick={() => onDleteExpense(expense.id)}
+                  className="border-2 border-red-800 p-1 rounded text-xl text-red-800 hover:bg-red-800 hover:text-red-100"
+                >
                   Delete
                 </button>
               </td>
@@ -40,8 +70,14 @@ const ExpenseList = ({ expenses, onDleteExpense }: Props) => {
         </tbody>
         <tfoot>
           <tr>
-            <th>Total</th>
-            <th>
+            <th
+              colSpan={2}
+              className="px-6 py-3 text-xl text-gray-600 font-bold"
+            >
+              Total
+            </th>
+            <th colSpan={2} className="px-6 py-3 text-xl text-gray-600">
+              $
               {expenses
                 .reduce((ac, expense) => ac + expense.amount, 0)
                 .toFixed(2)}
