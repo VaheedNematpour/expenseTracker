d3b9bb9c05e145d922bd7ed123301b7307c65116
Vaheed Nematpour <vaheed.nematpour@gmail.com>
2024-10-25 15:38:13 +0330

    Add style to ExpenseFilter component

--- frontEnd/src/components/ExpenseTracker/ExpenseFilter.tsx
+++ frontEnd/src/components/ExpenseTracker/ExpenseFilter.tsx
@@ -8,19 +8,24 @@ interface Props {
 
 const ExpenseFilter = ({ onSelectCategory }: Props) => {
   const [expanded, setExpanded] = useState(false);
+  const [categoryTitle, setCategoryTitle] = useState("");
 
   return (
     <>
       <h2 className="text-3xl text-gray-800 font-bold my-4">Expense Filter</h2>
 
-      <div>
+      <div className="border-2 border-gray-800 py-2 rounded">
         <div
           className="flex items-center justify-between"
           onClick={() => setExpanded(!expanded)}
         >
-          <p>All Category</p>
+          <p className="px-6 py-2 text-xl text-gray-600 font-medium">
+            {categoryTitle ? categoryTitle : "All Categories"}
+          </p>
 
-          {expanded ? <FaChevronUp /> : <FaChevronDown />}
+          <button className="px-6 py-2">
+            {expanded ? <FaChevronUp size={20} /> : <FaChevronDown size={20} />}
+          </button>
         </div>
 
         {expanded && (
@@ -28,16 +33,20 @@ const ExpenseFilter = ({ onSelectCategory }: Props) => {
             <li
               onClick={() => {
                 onSelectCategory("");
+                setCategoryTitle("");
                 setExpanded(false);
               }}
+              className="px-6 py-4 hover:bg-gray-200"
             ></li>
             {categories.map((category) => (
               <li
                 key={category}
                 onClick={() => {
                   onSelectCategory(category);
+                  setCategoryTitle(category);
                   setExpanded(false);
                 }}
+                className="px-6 py-2 hover:bg-gray-200"
               >
                 {category}
               </li>
