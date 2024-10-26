224ec3e6b689ded2eab3d8e29aede3c6ef76610f
Vaheed Nematpour <vaheed.nematpour@gmail.com>
2024-10-26 14:37:52 +0330

    Implement style toExpenseFilter component

--- frontEnd/src/components/ExpenseTracker/ExpenseFilter.tsx
+++ frontEnd/src/components/ExpenseTracker/ExpenseFilter.tsx
@@ -1,4 +1,5 @@
-import { FaChevronDown } from "react-icons/fa";
+import { useState } from "react";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
 interface Categories {
   id: number;
@@ -11,30 +12,50 @@ interface Props {
 }
 
 function ExpenseFilter({ categories, onSelectCategory }: Props) {
+  const [expanded, setExpanded] = useState(false);
+  const [categoryTitle, setCategoryTitle] = useState("All Categories");
+
   return (
     <>
-      <h2>Expense Filter</h2>
+      <h2 className="text-3xl text-gray-800 font-bold my-4">Expense Filter</h2>
 
-      <div>
-        <div className="flex items-center space-x-6">
-          <p>All Categories</p>
+      <div className="border-2 border-gray-800 rounded">
+        <div
+          className="flex items-center justify-between space-x-6 px-6 py-2"
+          onClick={() => setExpanded(!expanded)}
+        >
+          <p className="text-xl text-gray-600 font-bold">{categoryTitle}</p>
 
           <button>
-            <FaChevronDown />
+            {expanded ? <FaChevronUp size={20} /> : <FaChevronDown size={20} />}
           </button>
         </div>
 
-        <ul>
-          <li onClick={() => onSelectCategory("")}></li>
-          {categories.map((category) => (
+        {expanded && (
+          <ul>
             <li
-              key={category.id}
-              onClick={() => onSelectCategory(category.title)}
-            >
-              {category.title}
-            </li>
-          ))}
-        </ul>
+              onClick={() => {
+                onSelectCategory("");
+                setCategoryTitle("All Categories");
+                setExpanded(false);
+              }}
+              className="px-6 py-4 hover:bg-gray-200"
+            ></li>
+            {categories.map((category) => (
+              <li
+                key={category.id}
+                onClick={() => {
+                  onSelectCategory(category.title);
+                  setCategoryTitle(category.title);
+                  setExpanded(false);
+                }}
+                className="px-6 py-4 text-lg text-gray-600 font-medium hover:bg-gray-200"
+              >
+                {category.title}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </>
   );
